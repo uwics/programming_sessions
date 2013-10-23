b0941f0996a75f7803933aeaaa940e97c2aa263e
Antonio Fearon <antonio.l.fearon@gmail.com>
2013-10-23 18:08:10 -0500

    no message

--- src/http_test.py
+++ src/http_test.py
@@ -1,5 +1,5 @@
 import urllib2
-response = urllib2.urlopen("http://bus.artuvic.com/account.json?id=1").read()
+response = urllib2.urlopen("http://bus.artuvic.com/account.json?id=3").read()
 
 print response
 
